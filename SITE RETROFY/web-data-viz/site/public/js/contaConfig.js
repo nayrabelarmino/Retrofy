ed56b4d167abade156776c769112b617177335c8
nayrabelarmino <nayra.belarmino@sptech.school>
2023-05-22 10:11:31 -0300

    Atualizando páginas

---
+++ SITE RETROFY/web-data-viz/site/public/js/contaConfig.js
@@ -0,0 +1,6 @@
+var btnExpandir = document.querySelector('#botao-expandir')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExpandir.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
