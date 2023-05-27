f8cdc13743675c72edef238c832040890827719a
nayrabelarmino <nayra.belarmino@sptech.school>
2023-05-27 20:16:00 -0300

    Banco de dados

--- BANCO DE DADOS/MER-RETROFY.sql
+++ BANCO DE DADOS/MER-RETROFY.sql
@@ -10,11 +10,33 @@ senha varchar(8)
 );
 
 CREATE TABLE pontuacao (
-idPontuacao int primary key auto_increment,
+idPontuacao int,
 pontuacao int,
 acertos int,
-categoria
+categoria varchar(20),
+fkUsuario int,
+constraint fkUsuario foreign key (fkUsuario) references usuario (idUsuario),
+constraint pkComposta primary key (idPontuacao, fkUsuario)
 );
 
+CREATE TABLE votacao (
+idVotacao int,
+fkUsuario int,
+constraint fkUsuario foreign key (fkUsuario) references usuario (idUsuario),
+fkPlaylist int,
+constraint fkPlaylist foreign key (fkPlaylist) references playlist (idPlaylist),
+constraint pkComposta primary key (idVotacao, fkPlaylist) 
+);
 
+CREATE TABLE genero (
+idGenero int primary key,
+nomeGenero varchar(15),
+constraint chkGenero check(nomeGenero in ('Pop', 'Rock', 'Post-Punk'))
+);
 
+CREATE TABLE playlist (
+idPlaylist int primary key auto_increment,
+nome varchar(50),
+fkGenero int,
+constraint fkGenero foreign key (fkGenero) references genero (idGenero)
+);
