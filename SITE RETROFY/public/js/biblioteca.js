8825dfe01f86f7b4bd38faa3bbfa257ada051a95
nayrabelarmino <nayra.belarmino@sptech.school>
2023-06-03 18:24:13 -0300

    Atualizando telas

--- SITE RETROFY/public/js/biblioteca.js
+++
@@ -1,6 +0,0 @@
-var btnExpandir = document.querySelector('#botao-expandir')
-var menuSide = document.querySelector('.menu-lateral')
-
-btnExpandir.addEventListener('click', function(){
-    menuSide.classList.toggle('expandir')
-})
